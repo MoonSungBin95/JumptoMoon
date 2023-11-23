63b469a5d8025f2acc4576000c0adba7b8819fe3
MoonSungBin95 <66113141+MoonSungBin95@users.noreply.github.com>
2023-11-23 16:19:20 +0900

    add array

---
+++ Reservation_Arr.java
@@ -0,0 +1,82 @@
+package Meeting_Room;
+
+public class Reservation_Arr {
+	//날짜
+	int day = 0;
+	
+	// 예약정보
+	String[] name = new String[8];
+	int[] number = new int[8];
+	int[] password = new int[8];
+	
+	public void Set_data(Reservation_Arr[][] Arr) {
+		Arr[0][0].name[0] = "Lim";
+		Arr[0][0].name[1] = "Lee";
+		Arr[0][0].name[2] = "park";
+		Arr[0][0].name[3] = "Chun";
+		Arr[0][0].name[4] = "Goo";
+		Arr[0][0].name[5] = "Kim";
+		Arr[0][0].number[5] = 4;	
+		Arr[0][0].name[6] = "Joo";
+		Arr[0][1].name[2] = "moon";
+		Arr[0][1].name[3] = "Park";
+		Arr[0][1].name[4] = "Sim";
+		Arr[0][1].name[5] = "Kim";
+		Arr[0][1].number[5] = 4;
+		Arr[0][1].name[7] = "Kim";
+		Arr[0][1].number[7] = 4;
+		Arr[1][4].name[6] = "kim";
+		Arr[1][4].number[6] = 3;
+		Arr[1][0].name[5] = "moon";
+		Arr[1][0].number[5] = 9;
+		Arr[0][4].name[3] = "park";
+		Arr[0][4].number[3] = 3;
+		Arr[3][4].name[4] = "Joo";
+		Arr[3][4].number[4] = 6;
+		Arr[0][6].name[0] = "Kwon";
+		Arr[0][6].number[0] = 5;
+		Arr[2][1].name[2] = "Kim";
+		Arr[2][1].number[2] = 4;
+		Arr[0][5].name[4] = "Sung";
+		Arr[0][5].number[4] = 4;
+		Arr[4][0].name[5] = "moon";
+		Arr[4][0].number[5] = 2;
+		Arr[2][1].name[1] = "Lee";
+		Arr[2][1].number[1] = 4;
+		Arr[2][5].name[6] = "Lim";
+		Arr[2][5].number[6] = 8;
+		Arr[2][3].name[7] = "Seo";
+		Arr[2][3].number[7] = 4;
+		Arr[0][4].name[7] = "Kim";
+		Arr[0][4].number[7] = 10;
+		Arr[1][0].name[4] = "moon";
+		Arr[1][0].number[4] = 6;
+		Arr[0][4].name[3] = "Kim";
+		Arr[0][4].number[3] = 8;
+		Arr[3][4].name[6] = "Kim";
+		Arr[3][4].number[6] = 3;
+		Arr[3][2].name[4] = "Kim";
+		Arr[3][2].number[4] = 3;
+		Arr[3][3].name[2] = "Kim";
+		Arr[3][3].number[2] = 6;
+		Arr[0][2].name[5] = "Kim";
+		Arr[0][2].number[5] = 7;
+		Arr[0][6].name[3] = "Kim";
+		Arr[0][6].number[3] = 4;
+		Arr[2][1].name[4] = "Kim";
+		Arr[2][1].number[4] = 4;
+		Arr[3][3].name[4] = "Kim";
+		Arr[3][3].number[4] = 4;
+		Arr[1][4].name[0] = "Kim";
+		Arr[1][4].number[0] = 1;
+		Arr[3][0].name[2] = "Kim";
+		Arr[3][6].number[2] = 1;
+		Arr[3][6].name[6] = "Kim";
+		Arr[3][6].number[6] = 8;
+		Arr[3][5].name[2] = "Kim";
+		Arr[1][5].number[2] = 4;
+		Arr[3][0].name[4] = "Kim";
+		Arr[3][0].number[4] = 4;
+		
+	}
+}
